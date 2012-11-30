377108c57d420ab5863c7116a1302304d6352d9c
filirom1 <filirom1@gmail.com>
2012-11-30 05:28:11 +0000

    test domains

---
+++ tests/35-domains.sh
@@ -0,0 +1,45 @@
+# force exit on error
+set -e
+
+
+export PATH=$PATH:/home/vagrant/openruko/client/
+
+function print {
+  echo -e "\n\e[1;36m$1\e[00m"
+}
+
+print "list domains. (should be empty)"
+expect <<EOF
+  spawn openruko domains --app keepgreen
+  expect "keepgreen has no domain names."
+  expect eof
+EOF
+
+print "add domain toto.com"
+expect <<EOF
+  spawn openruko domains:add toto.com --app keepgreen
+  expect "Adding toto.com to keepgreen... done"
+  expect eof
+EOF
+
+print "list domains. (should find toto.com)"
+expect <<EOF
+  spawn openruko domains --app keepgreen
+  expect "=== keepgreen Domain Names"
+  expect "toto.com"
+  expect eof
+EOF
+
+print "remove domain toto.com"
+expect <<EOF
+  spawn openruko domains:remove toto.com --app keepgreen
+  expect "Removing toto.com from keepgreen... done"
+  expect eof
+EOF
+
+print "list domains. (should be empty)"
+expect <<EOF
+  spawn openruko domains --app keepgreen
+  expect "keepgreen has no domain names."
+  expect eof
+EOF
