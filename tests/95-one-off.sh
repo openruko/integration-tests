ba1acc514034068edeff67f4fa69914d3e024ae3
Thomas Buckley-Houston <tom@tombh.co.uk>
2013-02-23 14:12:33 +0000

    Increase expect timeouts on one-off tests

--- tests/95-one-off.sh
+++ tests/95-one-off.sh
@@ -1,6 +1,6 @@
 print "slotbox run ls ~/"
 expect <<EOF
-  set timeout 3 
+  set timeout 10 
   spawn slotbox run cat Procfile --app slotbox-nodejs-hello-world
   expect "attached to terminal... up, run.1"
   expect "web: node index.js"
@@ -9,7 +9,7 @@ EOF
 
 print "slotbox run 'env'"
 expect <<EOF
-  set timeout 3 
+  set timeout 10
   spawn slotbox run 'env' --app slotbox-nodejs-hello-world
   expect "KEY1=VALUE2"
   expect eof
